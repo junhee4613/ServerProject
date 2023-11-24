14cf53fa3253d798393c480df5a41a47e336a000
junhee4613 <junhee000123@naver.com>
2023-11-25 03:55:58 +0900

    Fix : leehan-node로 옮김

--- LeeHan/Assets/02.Scripts/MainScreen/MainButtonManager.cs
+++ LeeHan/Assets/02.Scripts/MainScreen/MainButtonManager.cs
@@ -19,7 +19,7 @@ public class MainButtonManager : MonoBehaviour
 
     public string token;
 
-    public const string apiUrl = "https://port-0-node-express-230927-jvpb2alnb1xslw.sel5.cloudtype.app";
+    public const string apiUrl = "https://port-0-leehan-node-20231014-jvpb2alnb1xslw.sel5.cloudtype.app";
 
     public InputField[] inputField;
     // Start is called before the first frame update
