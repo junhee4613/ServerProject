f9e073d3b5a26709fddaf83609dc5179d12f91ee
gkswhddnr <gkswhddnr7593@naver.com>
2023-11-24 10:42:14 +0900

    feat: 플레이어 HP & 피격 & 죽음 구현

--- LeeHan/Assets/02.Scripts/GameTest/TreelineMover.cs
+++ LeeHan/Assets/02.Scripts/GameTest/TreelineMover.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 
 public class TreelineMover : MonoBehaviour
 {
-    public float maxDistance = 22f;
+    public float maxDistance = 30f;
 
     // Start is called before the first frame update
     void Start()
