e6cbbdf8243aa2fd5903100687e49adb96c5baa6
junhee4613 <junhee000123@naver.com>
2023-11-27 12:39:33 +0900

    test : 웹소켓 테스트

--- LeeHan/Assets/02.Scripts/GameTest/Managers/GameManager.cs
+++ LeeHan/Assets/02.Scripts/GameTest/Managers/GameManager.cs
@@ -3,7 +3,6 @@ using System.Collections.Generic;
 using UnityEngine;
 using System;
 using UnityEngine.SceneManagement;
-using WebSocketSharp;
 using Newtonsoft.Json;
 using System.Text;
 using UnityEngine.UI;
@@ -15,22 +14,20 @@ public class GameManager : MonoBehaviour
     public Action game_over;
     public Action restart;
     public float time;
+    public string record_scord;
     UIController UIManager => UIController.instance;
+    WebSocketManager Web => WebSocketManager.instance;
 
     #region 서버와 관련된 것들
-    WebSocket webSocket;
-    bool IsConnected = false;
-    int ConnectionAttempt = 0;
-    const int MaxConnectionAttempts = 3;
+    
     public int loopCount;
     public MyData sendData = new MyData { message = "메세지 보내기" };
-    public float maximum_record = 0;
     [Serializable]
     public class MyData     //JSON 데이터를 위한 클래스
     {
         public string player_name;
         public string id;
-        public float my_maximum_record = 0;
+        public float maximum_record = 0;
         public string clientID;
         public string message;
         public string account_name;
@@ -46,79 +43,7 @@ public class GameManager : MonoBehaviour
         public string room;
         public int loopTimeCount;
     }
-    void ConnectWebSock()
-    {
-        webSocket = new WebSocket("ws://localhost:3000");           //3000포트에 연결
-        webSocket.OnOpen += OnWebSocketOpen;                        //함수 이벤트 등록
-        webSocket.OnMessage += OnWebSocketMessage;                        //함수 이벤트 등록
-        webSocket.OnClose += OnWebSocketClose;                        //함수 이벤트 등록
-
-        webSocket.ConnectAsync();
-    }
-    void OnWebSocketOpen(object sender, System.EventArgs e)
-    {
-        Debug.Log("WebSocket connected");
-        IsConnected = true;
-        ConnectionAttempt = 0;
-    }
-    void OnWebSocketMessage(object sender, MessageEventArgs e)
-    {
-        string jsonData = Encoding.Default.GetString(e.RawData);
-        Debug.Log("Received JSON data : " + jsonData);
-
-        //JSON 데이터를 객체로 역직렬화
-        MyData receiveData = JsonConvert.DeserializeObject<MyData>(jsonData);
-
-        InfoData infoData = JsonConvert.DeserializeObject<InfoData>(jsonData);
-        //여기가 최대 기록 할당 시키는 로직
-        UIManager.maximum_record_text.text = sendData.player_name + ":" + sendData.message;
-        if (infoData != null)
-        {
-            string room = infoData.myParams.room;
-            loopCount = infoData.myParams.loopTimeCount;
-        }
-
-        if (receiveData != null && !string.IsNullOrEmpty(receiveData.clientID))
-        {
-            sendData.clientID = receiveData.clientID;
-        }
-    }
-    void OnWebSocketClose(object sender, CloseEventArgs e)
-    {
-        Debug.Log("WebSocket connection closed");
-        IsConnected = false;
-
-        if (ConnectionAttempt < MaxConnectionAttempts)
-        {
-            ConnectionAttempt++;
-            Debug.Log("Attemping to reconnect . Attemp" + ConnectionAttempt);
-            ConnectWebSock();
-        }
-        else
-        {
-            Debug.Log("Failed to connect ager " + MaxConnectionAttempts + "attempts. ");
-        }
-    }
-    void OnAplicationQuit()             //어플리케이션 종료되면 소캣 제거
-    {
-        DisconnectWebSocket();
-    }
-    void DisconnectWebSocket()
-    {
-        if (webSocket != null && IsConnected)
-        {
-            webSocket.Close();
-            IsConnected = false;
-        }
-    }
-    public void SendSocketMessage()
-    {
-        sendData.requestType = 0;
-        sendData.message = time.ToString("F2");
-        string jsonData = JsonConvert.SerializeObject(sendData);
-
-        webSocket.Send(jsonData);
-    }
+    
     #endregion
 
 
@@ -146,7 +71,6 @@ public class GameManager : MonoBehaviour
         {
             pos_init = player.transform.position;
         }
-        ConnectWebSock();
     }
 
     // Update is called once per frame
@@ -157,21 +81,13 @@ public class GameManager : MonoBehaviour
             time += Time.deltaTime;
         }
     }
-    public void Destination_Arrival()
-    {
-        if (webSocket == null || !IsConnected)
-        {
-            return;
-        }
-        if (maximum_record < time)
-        {
-            SendSocketMessage();
-        }
-    }
+    
     public void Player_die()
     {
         die = true;
         Time.timeScale = 0;
+        //이건 테스트용
+        Destination_Arrival();
     }
     public void Game_restart()
     {
@@ -182,5 +98,13 @@ public class GameManager : MonoBehaviour
         Time.timeScale = 1;
         die = false;
         player.transform.position = pos_init;
+    }
+    public void Destination_Arrival()
+    {
+        Debug.Log("기록");
+        if (sendData.maximum_record < time)
+        {
+            Web.SendSocketMessage();
+        }
     }
 }
---
+++ LeeHan/Assets/02.Scripts/GameTest/Managers/WebSocketManager.cs
@@ -0,0 +1,114 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using WebSocketSharp;
+using System.Text;
+using Newtonsoft.Json;
+
+public class WebSocketManager : MonoBehaviour
+{
+    //remove.bg <= 뒷배경 제거해주는 사이트
+    public WebSocket webSocket;
+    bool IsConnected = false;
+    int ConnectionAttempt = 0;
+    const int MaxConnectionAttempts = 3;
+    UIController UIManager => UIController.instance;
+
+    GameManager GameManager => GameManager.instance;
+    public static WebSocketManager instance;
+    private void Awake()
+    {
+        if(instance == null)
+        {
+            instance = this;
+        }
+        else
+        {
+            Destroy(this.gameObject);
+        }
+        DontDestroyOnLoad(this);
+        
+    }
+    private void Start()
+    {
+        ConnectWebSock();
+    }
+    void ConnectWebSock()
+    {
+        webSocket = new WebSocket("ws://localhost:8000");           //3000포트에 연결
+        webSocket.OnOpen += OnWebSocketOpen;                        //함수 이벤트 등록
+        webSocket.OnMessage += OnWebSocketMessage;                        //함수 이벤트 등록
+        webSocket.OnClose += OnWebSocketClose;                        //함수 이벤트 등록
+
+        webSocket.ConnectAsync();
+    }
+    void OnWebSocketOpen(object sender, System.EventArgs e)
+    {
+        Debug.Log("WebSocket connected");
+        IsConnected = true;
+        ConnectionAttempt = 0;
+    }
+    void OnWebSocketMessage(object sender, MessageEventArgs e)
+    {
+        string jsonData = Encoding.Default.GetString(e.RawData);
+        Debug.Log("Received JSON data : " + jsonData);
+
+        //JSON 데이터를 객체로 역직렬화
+        GameManager.MyData receiveData = JsonConvert.DeserializeObject<GameManager.MyData>(jsonData);
+
+        GameManager.InfoData infoData = JsonConvert.DeserializeObject<GameManager.InfoData>(jsonData);
+        //테스트용@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@@
+        Debug.Log(receiveData.maximum_record);
+        UIManager.maximum_record_text.text = receiveData.player_name + ":" + receiveData.maximum_record.ToString("F2");
+
+        //여기가 최대 기록 할당 시키는 로직
+        //UIManager.maximum_record_text.text = GameManager.sendData.player_name + ":" + GameManager.sendData.message;
+        if (infoData != null)
+        {
+            string room = infoData.myParams.room;
+            GameManager.loopCount = infoData.myParams.loopTimeCount;
+        }
+
+        if (receiveData != null && !string.IsNullOrEmpty(receiveData.clientID))
+        {
+            GameManager.sendData.clientID = receiveData.clientID;
+        }
+    }
+    void OnWebSocketClose(object sender, CloseEventArgs e)
+    {
+        Debug.Log("WebSocket connection closed");
+        IsConnected = false;
+
+        if (ConnectionAttempt < MaxConnectionAttempts)
+        {
+            ConnectionAttempt++;
+            Debug.Log("Attemping to reconnect . Attemp" + ConnectionAttempt);
+            ConnectWebSock();
+        }
+        else
+        {
+            Debug.Log("Failed to connect ager " + MaxConnectionAttempts + "attempts. ");
+        }
+    }
+    void OnAplicationQuit()             //어플리케이션 종료되면 소캣 제거
+    {
+        DisconnectWebSocket();
+    }
+    void DisconnectWebSocket()
+    {
+        if (webSocket != null && IsConnected)
+        {
+            webSocket.Close();
+            IsConnected = false;
+        }
+    }
+    public void SendSocketMessage()
+    {
+        GameManager.sendData.requestType = 0;
+        GameManager.sendData.maximum_record = GameManager.time;
+        Debug.Log(GameManager.sendData.maximum_record + "최대기록");
+        string jsonData = JsonConvert.SerializeObject(GameManager.sendData);
+        webSocket.Send(jsonData);
+    }
+    
+}
